e87b4825462f4454d5cac0436228a2c62ccd7bc3
NG JIE RU <132632965+NGJIERU@users.noreply.github.com>
2026-01-01 17:23:58 +0800

    infra: provision VPC and Subnets via Terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,3 +10,19 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_subnets
+}
+
