2748eaec6b826da088b91555a0206ee0f3acc55b
NG JIE RU <132632965+NGJIERU@users.noreply.github.com>
2026-01-01 17:33:39 +0800

    chore: cleanup terraform state from git and finalize week2

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -26,3 +26,14 @@ output "public_subnets" {
   value       = module.vpc.public_subnets
 }
 
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = module.eks.cluster_name
+}
+
+
